c77ff6554f18e0ee760bf961eee35c26da4161e9
Iaroslav Mokroguz <maustrauk@gmail.com>
2020-11-09 15:48:24 -0500

    Task_2 Add Functionality

--- client/src/App.js
+++ client/src/App.js
@@ -38,7 +38,7 @@ export default function App () {
         <MovieList movies={movieList}/>
       </Route>
 
-      <Route path={`/movies`}>
+      <Route path={`/movies/:id`}>
         <Movie />
       </Route>
     </div>
