b1f057a72e03071c805dbad495f0295af92c6509
Iaroslav Mokroguz <maustrauk@gmail.com>
2020-11-09 18:32:44 -0500

    Task_3 Turn your Saved Movie list into s

--- client/src/Movies/SavedList.js
+++ client/src/Movies/SavedList.js
@@ -3,11 +3,14 @@ import {Link} from 'react-router-dom';
 
 export default function SavedList(props) {
   console.log(props);
+
   return (
     <div className="saved-list">
       <h3>Saved Movies:</h3>
       {props.list.map(movie => (
-        <span className="saved-movie">{movie.title}</span>
+        <Link to={`/movies/${movie.id}`} className="linkComponent">
+          {movie.title}
+          </Link>
       ))}
       <Link to="/" className="linkComponent">
         <div className="home-button">Home</div>
